6ccf3694398ff41a31dbbc3f57c2a064b34d39b2
guinmoon <guinmoon@gmail.com>
2025-03-14 22:21:23 +0300

    onGyrojs fix

--- data/js/events/onGyroAcc.js
+++ data/js/events/onGyroAcc.js
@@ -1,9 +1,9 @@
 onGyroAcc = function (gyroX, gyroY, gyroZ, gyroDirection, accx, accy, accz,accDirection) {
     log_d("GYRO: " + gyroX + ", " + gyroY + ", " + gyroZ + ", " + gyroDirection + ", " + accx + ", " + accy + ", " + accz + ", " + accDirection);
-    if (gyroDirection == 8 || gyroDirection == 10) {
-        lulu_leftHand_long(4,6);    
-    }
-    if (gyroDirection == 8 || gyroDirection == 10) {
+    if (gyroDirection == 8 || gyroDirection == 10) {           
         lulu_rightHand_long(4,6);
+    }
+    if (gyroDirection == 7 || gyroDirection == 9) {
+        lulu_leftHand_long(4,6); 
     }
 }
